9d840417f40fc8bce4eb2eb6094100c0f2c9e3ab
Alejandro Martin Herrera <amarher3012@fp.iesromerovargas.com>
2025-02-07 19:08:38 +0100

    Update CreateCharacterForm to use ModelForm instead of Form

--- tierra_media/forms.py
+++ tierra_media/forms.py
@@ -1,19 +1,8 @@
 from django import forms
-from .models import Character, Faction, Location, Race
+from .models import Character
 
 
 class CreateCharacterForm(forms.ModelForm):
-    name = forms.CharField(max_length=50, required=True)
-    faction = forms.ModelChoiceField(
-        label="Facción", queryset=Faction.objects.all(), required=True
-    )
-    location = forms.ModelChoiceField(
-        label="Ubicacion", queryset=Location.objects.all(), required=True
-    )
-    race = forms.ModelChoiceField(
-        label="Raza", queryset=Race.objects.all(), required=True
-    )
-
     class Meta:
         model = Character
-        fields = ["name", "faction", "location", "race"]
+        fields = ["name", "faction", "location", "race", "user"]
