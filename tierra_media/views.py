220e309a694f33c62fe950778916313565bcf083
Renato <rromval599@g.educaand.es>
2025-02-10 00:34:05 +0100

    Crear register.html. Comenzar vista de "register".

--- tierra_media/views.py
+++ tierra_media/views.py
@@ -1,7 +1,12 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+
+def register(request):
+    pass
 
 @login_required
 def index(request):
