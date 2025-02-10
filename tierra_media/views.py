1c59555deb90ace76a886e82dc6ba7dfa9f72327
Alejandro Martin Herrera <amarher3012@fp.iesromerovargas.com>
2025-02-10 09:49:41 +0100

    Add same name - different user check for character creation

--- tierra_media/views.py
+++ tierra_media/views.py
@@ -1,3 +1,4 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.views.generic import *
 from .forms import CreateCharacterForm
@@ -7,15 +8,24 @@ from .models import Character
 
 
 class CharacterCreation(FormView):
-    template_name = "character-creation.html"
+    template_name = "tierra_media/character-creation.html"
     form_class = CreateCharacterForm
     success_url = "/tierra-media/character-creation/success/"
 
+    def check_name(self, form):
+        name = form.cleaned_data.get("name")
+        user = form.cleaned_data.get("user")
+        if Character.objects.filter(name=name).filter(user=user).exists():
+            # TODO: add error message / render error page
+            return False
+        return True
+
     def form_valid(self, form):
-        form.save()
-        return super().form_valid(form)
+        if self.check_name(form):
+            form.save()
+            return super().form_valid(form)
+        return super().form_invalid(form)
 
 
 class CharacterCreationSuccess(TemplateView):
-    template_name = "success.html"
-    # TODO: Add wait + redirect back to the menu
+    template_name = "tierra_media/success.html"
