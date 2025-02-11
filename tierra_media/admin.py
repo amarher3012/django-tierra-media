3874c1e117a6f51dfb4e3b181b3a593a452195e9
Alejandro Martin Herrera <amarher3012@fp.iesromerovargas.com>
2025-02-11 08:40:55 +0100

    Revert "Feature/register"

--- tierra_media/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-# Register your models here.
-
-admin.site.register(
-    [Character, Faction, Location, Relationship, Race, Backpack, Weapon, Armor]
-)
--- tierra_media/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TierraMediaConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "tierra_media"
